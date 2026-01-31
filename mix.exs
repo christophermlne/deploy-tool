ad4a4b31ca4ebce76c225eed3c0fcbcc9b73dae9
Christopher Milne <christopher.milne@gmail.com>
2026-01-31 15:28:36 -0500

    mix.exs

--- mix.exs
+++ mix.exs
@@ -7,24 +7,49 @@ defmodule Deploy.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Deploy.Application, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      # Saga orchestrator from Ash
       {:reactor, "~> 1.0"},
+
+      # HTTP client
+      {:req, "~> 0.5"},
+
+      # JSON parsing (usually included with Req, but explicit is good)
+      {:jason, "~> 1.4"},
+
+      # For Slack webhooks
+      {:httpoison, "~> 2.0"},
+
+      # For development/testing
+      # {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
+      {:mox, "~> 1.0", only: :test},
+
       {:igniter, "~> 0.6", only: [:dev, :test]}
+
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp aliases do
+    [
+      setup: ["deps.get"],
+      deploy: ["run -e 'Deploy.Runner.setup()'"]
     ]
   end
 end
